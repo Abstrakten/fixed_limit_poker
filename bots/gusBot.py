453c42549fcf3157ab612fe709c3105f7d9a5a63
NCLAN\kter <kter@netcompany.com>
2021-11-01 19:30:33 +0100

    new gus, who dis

--- bots/gusBot.py
+++ bots/gusBot.py
@@ -1,4 +1,4 @@
-"""Example1 player"""
+"""The one true Gus"""
 from typing import Dict, Sequence
 
 from bots.BotInterface import BotInterface
@@ -7,58 +7,66 @@ from environment.Observation import Observation
 from utils.handValue import getHandPercent, getHandType, getHighestSuitCount, getLongestStraight
 
 
-class PercentBot(BotInterface):
+class GusBot(BotInterface):
 
-    def __init__(self, name="percentBot"):
+    def __init__(self, name="GusBot"):
         super().__init__(name=name)
 
     def act(self, action_space: Sequence[Action], observation: Observation) -> Action:
         stage = observation.stage
         if stage == Stage.PREFLOP:
-            return self.getNearestAction(self.handlePreFlop(observation), action_space)
+            return self.getNearestAction(self.handlePreFlop(observation, action_space), action_space)
         elif stage == Stage.FLOP:
-            return self.getNearestAction(self.handleFlop(observation), action_space)
+            return self.getNearestAction(self.handleFlop(observation, action_space), action_space)
         elif stage == Stage.TURN:
-            return self.getNearestAction(self.handleTurn(observation), action_space)
+            return self.getNearestAction(self.handleTurn(observation, action_space), action_space)
         elif stage == Stage.RIVER:
-            return self.getNearestAction(self.handleRiver(observation), action_space)
-        
+            return self.getNearestAction(self.handleRiver(observation, action_space), action_space)
+
         # Unexpected!
         return Action.FOLD
 
-    def handlePreFlop(self, observation: Observation) -> Action:
+    def handlePreFlop(self, observation: Observation, action_space: Sequence[Action]) -> Action:
         handPercent, _ = getHandPercent(observation.myHand)
         if handPercent < .40:
             return Action.RAISE
-        elif handPercent < .60:
+        elif handPercent < .70:
             return Action.CALL
+        elif Action.CHECK in action_space:
+            return Action.CHECK
         return Action.FOLD
 
-    def handleFlop(self, observation: Observation) -> Action:
+    def handleFlop(self, observation: Observation, action_space: Sequence[Action]) -> Action:
         handPercent, cards = getHandPercent(
             observation.myHand, observation.boardCards)
-        if handPercent <= .60:
+        if handPercent <= .20:
             return Action.RAISE
-        elif handPercent <= .80 or self.getFlushDraw(observation) or self.getStraightDraw(observation):
+        elif handPercent <= .60 or self.getFlushDraw(observation) or self.getStraightDraw(observation):
             return Action.CALL
+        elif Action.CHECK in action_space:
+            return Action.CHECK
         return Action.FOLD
 
-    def handleTurn(self, observation: Observation) -> Action:
+    def handleTurn(self, observation: Observation, action_space: Sequence[Action]) -> Action:
         handPercent, cards = getHandPercent(
             observation.myHand, observation.boardCards)
         if handPercent <= .50:
             return Action.RAISE
         elif handPercent <= .70 or self.getFlushDraw(observation) or self.getStraightDraw(observation):
             return Action.CALL
+        elif Action.CHECK in action_space:
+            return Action.CHECK
         return Action.FOLD
 
-    def handleRiver(self, observation: Observation) -> Action:
+    def handleRiver(self, observation: Observation, action_space: Sequence[Action]) -> Action:
         handPercent, cards = getHandPercent(
             observation.myHand, observation.boardCards)
         if handPercent <= .40:
             return Action.RAISE
         elif handPercent <= .60:
             return Action.CALL
+        elif Action.CHECK in action_space:
+            return Action.CHECK
         return Action.FOLD
 
     def getNearestAction(self, action: Action, actionSpace: Sequence[Action]) -> Action:
@@ -69,10 +77,11 @@ class PercentBot(BotInterface):
         return action
 
     def getFlushDraw(self, observation: Observation):
-        count, suit = getHighestSuitCount(observation.myHand, observation.boardCards)
+        count, suit = getHighestSuitCount(
+            observation.myHand, observation.boardCards)
         return count == 4
 
     def getStraightDraw(self, observation: Observation):
-        count, lowRank, highRank = getLongestStraight(observation.myHand, observation.boardCards)
+        count, lowRank, highRank = getLongestStraight(
+            observation.myHand, observation.boardCards)
         return count == 4
-
